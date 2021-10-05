a578b5dd37498a6b408b4bc5d7bda4e92912549a
LexMillins <LKM892@student.bham.ac.uk>
2021-10-05 11:54:18 +0100

    Comments added to code

--- first_go.py
+++ first_go.py
@@ -1,17 +1,17 @@
-import ROOT
+import ROOT #import ROOT in python and run script in python to use pyROOT
 import numpy as np
 import scipy as scipy
 from ROOT import TCanvas
 
-f = ROOT.TFile.Open("~/Y4Proj/user.achishol.18108467._000001.output.root", "read")
+f = ROOT.TFile.Open("~/Y4Proj/user.achishol.18108467._000001.output.root", "read") #TFile directory
 
 f.ls()
 
-tree = f.Get("nominal")
+tree = f.Get("nominal") #extract tree 
 
-c = ROOT.TCanvas("myCanvasName", "The Canvas Title", 400, 300)
-tree.Draw("el_pt","el_pt")
+c = ROOT.TCanvas("myCanvasName", "The Canvas Title", 400, 300) #create a canvas for Histogram
+tree.Draw("el_pt","el_pt") #Fill canvas with selected data
 
-c.Draw()
+c.Draw() #plot histogram
 c.Show()
 
