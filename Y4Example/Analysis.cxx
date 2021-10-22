a9f17b7e29e451aae1598e5ba8c06e80b86c3221
LexMillins <LKM892@student.bham.ac.uk>
2021-10-22 15:45:53 +0100

    Muons, Electrons, Jets, and DiZs all reconstructing in generalized code

--- Y4Example/Analysis.cxx
+++ Y4Example/Analysis.cxx
@@ -1,6 +1,8 @@
 #include "Analysis.h"
 #include <algorithm>
 
+
+//print function for vectors of float
 void print_vect_float(std::vector <float> const &a) {
    std::cout << "The vector elements are : ";
 
@@ -9,22 +11,18 @@ void print_vect_float(std::vector <float> const &a) {
    }
 }
 
+
+//print function for Pt in vectors of TLorentzVectors
 void print_vect_TLor(std::vector <TLorentzVector> const &b) {
    std::cout << "The vector elements are : ";
-
+ 
    for(int i=0; i < b.size(); i++){
    std::cout << b.at(i).Pt() << std::endl;
    }
 }
-//bool sortby_jet_pt(std::vector<double> &lhs, std::vector<double> &rhs){
-//	return lhs.at(0) > rhs.at(0);
-//}
 
-//bool sortby_jet_pt(std::vector<TLorentzVector> &lhs, std::vector<TLorentzVector> &rhs){
-//	return lhs.at(0).Pt() > rhs.at(0).Pt();
-//}
-
-bool sortby_jet_pt(const TLorentzVector &lhs, const TLorentzVector &rhs){
+//function to sort TLorentzVectors by transverse momentum
+bool sortby_pt(const TLorentzVector &lhs, const TLorentzVector &rhs){
 	return lhs.Pt() > rhs.Pt();
 }
 
@@ -38,7 +36,7 @@ int main(int argc, char* argv[]) {
 
 	// Open the file and setup the reader class
 
-	TFile* inputFile = TFile::Open("user.achishol.18108467._000001.output.root");
+	TFile* inputFile = TFile::Open("ZZ.root");
 
 	TTree* tree = (TTree*) inputFile->Get("nominal");
 
@@ -55,7 +53,9 @@ int main(int argc, char* argv[]) {
 
 	TH1D* h_DiElectron_Mass = new TH1D("h_DiElectron_mass","; Mass [GeV]; Events / GeV",100,50,150);
 
-	TH1D* h_Dijet_mass = new TH1D("h_Dijet_Mass", "Mass [GeV]; Events/GeV",100,50,150);
+	TH1D* h_Dijet_mass = new TH1D("h_Dijet_Mass", "Mass [GeV]; Events/GeV",100,0,300);
+
+	TH1D* h_DiZ_mass = new TH1D("h_DiZ_Mass", "; Mass [GeV]; Events /GeV", 100, 50, 750);
 
 	// Event loop
 
@@ -73,6 +73,7 @@ int main(int argc, char* argv[]) {
 
 
 		std::vector<TLorentzVector> my_jets;
+		TLorentzVector dijet;
 
 
 
@@ -87,131 +88,79 @@ int main(int argc, char* argv[]) {
 		}
 
 
-		std::sort(my_jets.begin(), my_jets.end(),sortby_jet_pt);
+		std::sort(my_jets.begin(), my_jets.end(),sortby_pt);
 
-		print_vect_TLor(my_jets);
+		//print_vect_TLor(my_jets);
 
 		if(my_jets.size() >= 2){
-		std::vector<TLorentzVector> biggest_jets;
-		biggest_jets.push_back(my_jets.at(0));
-		biggest_jets.push_back(my_jets.at(1));
-		
-		TLorentzVector dijet = my_jets.at(0) + my_jets.at(1);
+			
+			dijet = my_jets.at(0) + my_jets.at(1);
 
-		h_Dijet_mass->Fill(dijet.M());
+			h_Dijet_mass->Fill(dijet.M());
 
 
 
 		}
-		//std::vector<TLorentzVector> biggest_jets;
-		//biggest_jets.push_back(my_jets.at(0));
-		//biggest_jets.push_back(my_jets.at(1));
-		
-		//TLorentzVector dijet = my_jets.at(0) + my_jets.at(1);
+		std::vector<TLorentzVector> muon_vector;
+		std::vector<TLorentzVector> electron_vector;
+		TLorentzVector dieelectron;
+		TLorentzVector dimuon;
 
-		//h_Dijet_mass->Fill(dijet.M());
-
-		//if( r->mu_pt->size() == 2) {
+		if( r->mu_pt->size() >= 2) {
 
 
-			//TLorentzVector muon1, muon2;
+			for( int k = 0; k<(r->mu_pt->size()); ++k){
+				TLorentzVector muon;
 
-			//muon1.SetPtEtaPhiM(r->mu_pt->at(0)*1e-3,r->mu_eta->at(0),r->mu_phi->at(0),105.67e-3);
-			//muon2.SetPtEtaPhiM(r->mu_pt->at(1)*1e-3,r->mu_eta->at(1),r->mu_phi->at(1),105.67e-3);
+				muon.SetPtEtaPhiM(r->mu_pt->at(k)*1e-3,r->mu_eta->at(k),r->mu_phi->at(k),105.67e-3);
+				muon_vector.push_back(muon);
 
-			//TLorentzVector dimuon = muon1 + muon2;
+			}
+			std::sort(muon_vector.begin(), muon_vector.end(), sortby_pt);
 
-			//h_DiMuon_Mass->Fill(dimuon.M());
-
-		//}
-
-		//else if ( r->el_pt->size() == 2)
-		//{
-			//TLorentzVector elec1, elec2;
-
-			//elec1.SetPtEtaPhiM(r->el_pt->at(0)*1e-3,r->el_eta->at(0),r->el_phi->at(0),0.511e-3);
-			//elec2.SetPtEtaPhiM(r->el_pt->at(1)*1e-3,r->el_eta->at(1),r->el_phi->at(1),0.511e-3);
-
-			//TLorentzVector dieelectron = elec1 + elec2;
-
-			//h_DiElectron_Mass->Fill(dieelectron.M());
-
-		//}
-
-		//if( r->mu_pt->size()>2){
-		//	std::vector<float> muon_vector = *(r->mu_pt);
-		//	print_vect_float(muon_vector);
+			dimuon = muon_vector.at(0) + muon_vector.at(1);
+			h_DiMuon_Mass->Fill(dimuon.M());
 
 
-		//}
-/*
-		if ( r->jet_pt->size() >= 2)
-		{
-
-			std::cout << "New Event" << std::endl;
-			std::vector<float> jet_pt_dummy = *(r->jet_pt);
-			//print_vect_float(jet_pt_dummy);
-
-			int p = r->jet_pt->size();
-			std::sort(jet_pt_dummy.begin(), jet_pt_dummy.begin()+p);
-
-			std::cout << "Array after sorting : \n" ;
-			print_vect_float(jet_pt_dummy);
-		
-
-			//return 0;
-
-			std::vector<vector<double>> jets_total;
-			std::vector<vector<double>> biggest_jets;
+		}
 
 
-				for( int j = 0; j <(r->jet_pt->size()); ++j){
+		else if ( r->el_pt->size() >= 2){
 
-					TLorentzVector jetj;
+			for(int j = 0; j<(r->el_pt->size()); ++j){
+				TLorentzVector elec;
 
-					jetj.SetPtEtaPhiE(r->jet_pt->at(j)*1e-3, r->jet_eta->at(j), r->jet_phi->at(j), r->jet_e->at(j)*1e-3);
-
-					std::vector<double> jet_vector;
-
-					std::vector<vector<double>> jets_total;
-
-						
-					for (int L=0; L<4; L++){
-	        			jet_vector.push_back(jetj(L));					
-					
-
-						jets_total.push_back(jet_vector);
-					}
-
-				for(int a = 0; a < p; ++a){
-
-					std::vector<double> v = jets_total.at(a);
-					std::vector<double> c = jets_total.at(a+1);
-
-					if (sortby_jet_pt(v, c) == 1){
-						biggest_jets.push_back(v);
-
-					}
-				}
+				elec.SetPtEtaPhiM(r->el_pt->at(j)*1e-3,r->el_eta->at(j),r->el_phi->at(j),0.511e-3);
+				electron_vector.push_back(elec);
 
 			}
 
-			//std::vector<double> jet1 = biggest_jets.at(0);
-			//std::vector<double> jet2 = biggest_jets.at(1);
-
-			//float inv_mass__squared_Z = 2*jet1.at(0)*jet2.at(0)*(cosh(jet1.at(1) - jet2.at(1)) - cos(jet1.at(2) - jet2.at(2)));
-			//float inv_mass_z = sqrt(inv_mass__squared_Z);
-			//h_Dijet_mass->Fill(inv_mass_z);
-
-
-
+			std::sort(electron_vector.begin(), electron_vector.end(), sortby_pt);
 			
+			dieelectron = electron_vector.at(0) + electron_vector.at(1);
 
-	
-
+			h_DiElectron_Mass->Fill(dieelectron.M());
 
 		}
-*/
+
+
+
+		TLorentzVector diZ;
+		if( dijet.Pt() != 0){
+
+			if (dimuon.Pt() !=0){
+				diZ = dijet + dimuon;
+			}
+
+			else if (dieelectron.Pt() !=0){
+
+				diZ = dijet + dieelectron;
+			}
+
+		h_DiZ_mass->Fill(diZ.M());
+		}
+
+
 	} // Event Loop
 
 
@@ -223,6 +172,7 @@ int main(int argc, char* argv[]) {
 	h_DiMuon_Mass->Write();
 	h_DiElectron_Mass->Write();
 	h_Dijet_mass->Write();
+	h_DiZ_mass->Write();
 
 	outputFile->Close();
 
