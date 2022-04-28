6fe1026fb6f07c99af827edcd4ed0ffa525bd91a
LexMillins <LKM892@student.bham.ac.uk>
2022-04-28 15:51:24 +0100

    Fitting MadGraph with Sherpa

--- Y4Example/MC_Fit.C
+++ Y4Example/MC_Fit.C
@@ -44,16 +44,16 @@ void MakeExample(TH1D* & h_Signal, TH1D* & h_Bkgd) {
 }
 
 
-void Fit() {
+void MC_Fit() {
 
     using namespace RooFit;
 
     TH1D* h_Signal = NULL;
     TH1D* h_Bkgd = NULL;
     TH1D* h_Data = NULL;
-    TH1D* h_ttbar = NULL;
-    TH1D* h_light_Bckgd = NULL;
-    TH1D* h_bb_bckgd = NULL;
+    TH1D* h_MG_Bkgd = NULL;
+    TH1D* h_MG_Sig = NULL;
+
 
     // For standalone example, make some "toy" signal and background histogram
     //MakeExample(h_Signal,h_Bkgd);
@@ -69,9 +69,11 @@ void Fit() {
 
     TString inputFileNameMG = "MadGraphplots.root";
 
-    TFile* inputFile = TFile::Open(inputFileNameMG);
+    TFile* inputFile = TFile::Open(inputFileName);
 
     TFile* inputFileData = TFile::Open(inputFileNameData);
+
+    TFile* inputFileMG = TFile::Open(inputFileNameMG);
 
     TString histNameData = "h_Z_mass";
 
@@ -79,31 +81,24 @@ void Fit() {
 
     TString histNameBkgd = "h_Bckgd";
 
-    TString histNamettBkgd = "h_ttbar";
-
-    TString histNamelightBkgd = "h_light_Bckgd";
-
-    TString histNamebbBkgd = "h_bb_Bckgd";
-
     h_Signal = (TH1D*)inputFile->Get(histNameSig);
 
     h_Bkgd = (TH1D*)inputFile->Get(histNameBkgd);
 
-    h_ttbar = (TH1D*)inputFile->Get(histNamettBkgd);
+    //h_Data = (TH1D*)inputFileData->Get(histNameData);
 
-    h_Data = (TH1D*)inputFileData->Get(histNameData);
+    h_MG_Sig = (TH1D*)inputFile->Get(histNameSig);
 
-    h_light_Bckgd = (TH1D*)inputFile->Get(histNamelightBkgd);
+    h_MG_Bkgd = (TH1D*)inputFile->Get(histNameBkgd);
 
-    h_bb_bckgd = (TH1D*)inputFile->Get(histNamebbBkgd);
     
     //----------
 
     // Make a "dummy" dataset from the expected data and background
     h_Data = (TH1D*) h_Bkgd->Clone("h_Data");
     h_Data->Reset();
-    h_Data->Add(h_Signal);
-    h_Data->Add(h_Bkgd);
+    h_Data->Add(h_MG_Sig);
+    h_Data->Add(h_MG_Bkgd);
 
     //----------
     // When you get the "real" data, you can simply load it from the file and
@@ -123,24 +118,14 @@ void Fit() {
 
     // Do the same for signal and background histograms, used to build PDFs
     RooDataHist m_Hist_Signal("m_Hist_Signal","",m_Mass,h_Signal);
-    //RooDataHist m_Hist_Bkgd("m_Hist_Bkgd","",m_Mass,h_Bkgd);
-    RooDataHist m_Hist_Bkgd_ttbar("m_Hist_Bkgd_ttbar","",m_Mass,h_ttbar);
-    RooDataHist m_Hist_Bkgd_light("m_Hist_Bkgd_light","",m_Mass,h_light_Bckgd);
-    RooDataHist m_Hist_Bkgd_bb("m_Hist_Bkgd_bb","",m_Mass,h_bb_bckgd);
-
+    RooDataHist m_Hist_Bkgd("m_Hist_Bkgd","",m_Mass,h_Bkgd);
+    
     RooHistPdf pdf_Signal("pdf_Signal","",m_Mass,m_Hist_Signal);
-    //RooHistPdf pdf_Bkgd("pdf_Bkgd","",m_Mass,m_Hist_Bkgd);
-    RooHistPdf pdf_Bkgd_ttbar("pdf_Bkgd_ttbar","",m_Mass,m_Hist_Bkgd_ttbar);
-    RooHistPdf pdf_Bkgd_light("pdf_Bkgd_light","",m_Mass,m_Hist_Bkgd_light);
-    RooHistPdf pdf_Bkgd_bb("pdf_Bkgd_bb","",m_Mass,m_Hist_Bkgd_bb);
-
+    RooHistPdf pdf_Bkgd("pdf_Bkgd","",m_Mass,m_Hist_Bkgd);
 
     // Signal Strength parameters for signal and background
     RooRealVar mu_Signal("mu_Signal","",1.0,-1000.0,1000.0);
-    //RooRealVar mu_Bkgd("mu_Bkgd","",1.0,-1000.0,1000.0);
-    RooRealVar mu_Bkgd_ttbar("mu_Bkgd_ttbar","",1.0,-1000.0,1000.0);
-    RooRealVar mu_Bkgd_light("mu_Bkgd_light","",1.0,-1000.0,1000.0);
-    RooRealVar mu_Bkgd_bb("mu_Bkgd_bb","",1.0,-1000.0,1000.0);
+    RooRealVar mu_Bkgd("mu_Bkgd","",1.0,-1000.0,1000.0);
 
     //mu_Bkgd.setConstant(kTRUE);
     //mu_Bkgd.setVal(1.0873e+00);
@@ -149,26 +134,17 @@ void Fit() {
 
     // Number of events (after weighting / scaling) for MC prediction
     RooRealVar N_Signal_MC("N_Signal_MC","",h_Signal->Integral());
-    //RooRealVar N_Bkgd_MC("N_Bkgd_MC","",h_Bkgd->Integral());
-    RooRealVar N_Bkgd_ttbar_MC("N_Bkgd_ttbar_MC","",h_ttbar->Integral());
-    RooRealVar N_Bkgd_light_MC("N_Bkgd_light_MC","",h_light_Bckgd->Integral());
-    RooRealVar N_Bkgd_bb_MC("N_Bkgd_bb_MC","",h_bb_bckgd->Integral());
-
+    RooRealVar N_Bkgd_MC("N_Bkgd_MC","",h_Bkgd->Integral());
+    
     // Our number of events in the fit for S and B: mu*N_Events for S and B, separately
     RooFormulaVar N_Signal("N_Signal","mu_Signal*N_Signal_MC",RooArgSet(mu_Signal,N_Signal_MC));
-    //RooFormulaVar N_Bkgd("N_Bkgd","mu_Bkgd*N_Bkgd_MC",RooArgSet(mu_Bkgd,N_Bkgd_MC));
-    RooFormulaVar N_Bkgd_ttbar("N_Bkgd_ttbar","mu_Bkgd_ttbar*N_Bkgd_ttbar_MC",RooArgSet(mu_Bkgd_ttbar,N_Bkgd_ttbar_MC));
-    RooFormulaVar N_Bkgd_light("N_Bkgd_light","mu_Bkgd_light*N_Bkgd_light_MC",RooArgSet(mu_Bkgd_light,N_Bkgd_light_MC));
-    RooFormulaVar N_Bkgd_bb("N_Bkgd_bb","mu_Bkgd_bb*N_Bkgd_bb_MC",RooArgSet(mu_Bkgd_bb,N_Bkgd_bb_MC));
-
+    RooFormulaVar N_Bkgd("N_Bkgd","mu_Bkgd*N_Bkgd_MC",RooArgSet(mu_Bkgd,N_Bkgd_MC));
+    
     // Build Poisson terms for N_Signal and N_Bkgd and associate them with relevant PDFs
     RooExtendPdf epdf_Signal("epdf_Signal","",pdf_Signal,N_Signal);
-    //RooExtendPdf epdf_Bkgd("epdf_Bkgd","",pdf_Bkgd,N_Bkgd);
-    RooExtendPdf epdf_Bkgd_ttbar("epdf_Bkgd_ttbar","",pdf_Bkgd_ttbar,N_Bkgd_ttbar);
-    RooExtendPdf epdf_Bkgd_light("epdf_Bkgd_light","",pdf_Bkgd_light,N_Bkgd_light);
-    RooExtendPdf epdf_Bkgd_bb("epdf_Bkgd_bb","",pdf_Bkgd_bb,N_Bkgd_bb);
+    RooExtendPdf epdf_Bkgd("epdf_Bkgd","",pdf_Bkgd,N_Bkgd);
 
-    RooAddPdf pdf_Total("pdf_Total","",RooArgList(epdf_Signal,epdf_Bkgd_bb, epdf_Bkgd_light, epdf_Bkgd_ttbar));
+    RooAddPdf pdf_Total("pdf_Total","",RooArgList(epdf_Signal,epdf_Bkgd));
 
     //mu_Bkgd.setConstant(kTRUE);
 
@@ -179,7 +155,7 @@ void Fit() {
     // Make a plot
     TCanvas* c = new TCanvas("c","",800,600);
 
-    RooPlot* frame = m_Mass.frame(Title("Invariant Mass of Z Fit Data (after b-tag)"));
+    RooPlot* frame = m_Mass.frame(Title("MadGraph fitted with Sherpa"));
 
 
     // Plot data and PDF overlaid, use expected number of events for p.d.f projection normalization
@@ -189,10 +165,7 @@ void Fit() {
 
     pdf_Total.plotOn(frame,Normalization(1.0,RooAbsReal::RelativeExpected),Name("Total")) ;
     // Overlay the background component of model with a dashed line
-    //pdf_Total.plotOn(frame,Components(epdf_Bkgd),LineStyle(kDashed),Normalization(1.0,RooAbsReal::RelativeExpected),Name("BOnly")) ;
-    pdf_Total.plotOn(frame,Components(epdf_Bkgd_light),LineColor(kGreen), LineStyle(kDashed),Normalization(1.0,RooAbsReal::RelativeExpected),Name("lightBOnly")) ;
-    pdf_Total.plotOn(frame,Components(epdf_Bkgd_bb),LineColor(kOrange), LineStyle(kDashed),Normalization(1.0,RooAbsReal::RelativeExpected),Name("bbBOnly")) ;
-    pdf_Total.plotOn(frame,Components(epdf_Bkgd_ttbar),LineStyle(kDotted),Normalization(1.0,RooAbsReal::RelativeExpected),Name("ttBOnly")) ;
+    pdf_Total.plotOn(frame,Components(epdf_Bkgd),LineStyle(kDashed),Normalization(1.0,RooAbsReal::RelativeExpected),Name("BOnly")) ;
     // Overlay the signal components
     pdf_Total.plotOn(frame,Components(RooArgSet(epdf_Signal)),LineColor(kRed),Normalization(1.0,RooAbsReal::RelativeExpected),Name("SOnly")) ;
 
@@ -207,10 +180,7 @@ void Fit() {
 
     Leg->AddEntry("Data", "Data" , "lep");
     Leg->AddEntry("Total", "Fit Result" , "l");
-    //Leg->AddEntry("BOnly", "Z+jets Bkgd. Component" , "l");
-    Leg->AddEntry("ttBOnly", "ttbar Bkgd. Component" , "l");
-    Leg->AddEntry("lightBOnly", "light Z+jets Bkgd. Component" , "l");
-    Leg->AddEntry("bbBOnly", "bb Z+jets Bkgd. Component" , "l");
+    Leg->AddEntry("BOnly", "Z+jets Bkgd. Component" , "l");
     Leg->AddEntry("SOnly", "Signal Component" , "l");
 
     Leg->Draw();
@@ -218,6 +188,6 @@ void Fit() {
 
     fit_result->Print();
 
-    c->SaveAs("FitResult.pdf");
+    c->SaveAs("MCFitResult.pdf");
 
 }
